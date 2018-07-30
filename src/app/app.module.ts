d7f3a66a674a5da115c2ad851a7409e8ef6a7a74
jdaro10 <jesusdari10@hotmail.com>
2018-07-30 17:36:59 -0500

    segundo commit resolver tarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 //rutas
 import { APP_ROUTES } from './app.routes';
 
+
+/*MODULOS*/
+import {PageModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
@@ -14,7 +18,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register/register.component';
+import { RegisterComponent } from './login/register.component';
 
 
 @NgModule({
@@ -22,18 +26,19 @@ import { RegisterComponent } from './login/register/register.component';
     AppComponent,
     LoginComponent,
     NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
+    //DashboardComponent,
+    //ProgressComponent,
+    //Graficas1Component,
+      //PagesComponent,
     HeaderComponent,
     SidebarComponent,
     BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register/register.component';
+import { RegisterComponent } from './login/register.component';
 
 const appRoutes : Routes = [
     {
