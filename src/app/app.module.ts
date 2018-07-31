caef0af6a3647691aa9d63a126d9b441c168bcb7
jdaro10 <jesusdari10@hotmail.com>
2018-07-31 10:53:13 -0500

    Modificaciones a los modulos para mejor organizacion v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,6 @@ import {PageModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -25,14 +17,6 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    //DashboardComponent,
-    //ProgressComponent,
-    //Graficas1Component,
-      //PagesComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
     RegisterComponent
   ],
   imports: [
